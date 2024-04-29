7750912dd63875803a5a3eea44ca385fd457b783
Haikal Risnandar <haikalrisnandar82@gmail.com>
2024-04-29 17:14:43 +0700

    menambahkan class Pembeli

---
+++ Tubes_KPL_Kelompok1/Pembeli.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes_KPL_Kelompok1
+{
+    public class Pembeli
+    {
+        public int a {  get; set; }
+        public Pembeli(int a) {
+            this.a = a;
+        }
+    }
+}
--- Tubes_KPL_Kelompok1/Program.cs
+++ Tubes_KPL_Kelompok1/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Tubes_KPL_Kelompok1;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Pembeli z = new Pembeli(10);
+        Console.WriteLine(z.a);
+    }
+}
