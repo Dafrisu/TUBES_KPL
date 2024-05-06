05bf3597b92547e82b2ed864aa4e1e13ca1b9afd
Dafrisu <daffaraymisuandi@gmail.com>
2024-05-06 10:00:30 +0700

    Delete aja

--- Tubes_KPL_Kelompok1/obj/Debug/net8.0/Tubes_KPL_Kelompok1.AssemblyInfo.cs
+++ Tubes_KPL_Kelompok1/obj/Debug/net8.0/Tubes_KPL_Kelompok1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tubes_KPL_Kelompok1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5e6176b14446e711361dec3f57db45a28fe74be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2029ef49ec8dee0fcd33fbd332d513a3b58c7057")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tubes_KPL_Kelompok1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tubes_KPL_Kelompok1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
